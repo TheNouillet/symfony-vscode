39c487f0b7678a265f28d609dbc8fba2da2e755a
Axel VEBER <axel.veber@gmail.com>
2018-08-01 23:01:56 +0200

    #10 Autocomplete of public services in PHP

--- src/mvc/AutocompleteController.ts
+++ src/mvc/AutocompleteController.ts
@@ -1,15 +1,18 @@
 import { ContainerStore } from "../symfony/ContainerStore";
 import * as vscode from "vscode"
 import { YAMLServiceProvider } from "./editing/YAMLServiceProvider";
+import { PHPServiceProvider } from "./editing/PHPServiceProvider";
 
 export class AutocompleteController {
     private _disposable: vscode.Disposable
 
     constructor(containerStore: ContainerStore) {
         let yamlServiceProvider = new YAMLServiceProvider(containerStore)
+        let phpServiceProvider = new PHPServiceProvider(containerStore)
 
         let disposables: vscode.Disposable[] = []
         disposables.push(vscode.languages.registerCompletionItemProvider({ scheme: 'file', language: 'yaml' }, yamlServiceProvider, "@"))
+        disposables.push(vscode.languages.registerCompletionItemProvider({ scheme: 'file', language: 'php' }, phpServiceProvider))
 
         this._disposable = vscode.Disposable.from(...disposables)
     }
---
+++ src/mvc/editing/PHPServiceProvider.ts
@@ -0,0 +1,28 @@
+import * as vscode from "vscode";
+import { ContainerStore } from "../../symfony/ContainerStore";
+
+export class PHPServiceProvider implements vscode.CompletionItemProvider {
+
+    private _containerStore: ContainerStore
+
+    constructor(containerStore: ContainerStore) {
+        this._containerStore = containerStore
+    }
+
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+        let result: vscode.CompletionItem[] = []
+        let serviceDefinitions = this._containerStore.serviceDefinitionList
+        serviceDefinitions.forEach(serviceDefinition => {
+            if(serviceDefinition.public === true && !serviceDefinition.id.match(/instanceof\./)) {
+                let item = new vscode.CompletionItem(serviceDefinition.id, vscode.CompletionItemKind.Reference)
+                item.detail = "Of class " + serviceDefinition.className
+                if(serviceDefinition.id.match(/^(?:\\{1,2}\w+|\w+\\{1,2})(?:\w+\\{0,2}\w+)+$/)) {
+                    // If the service Id is a class, we append ::class
+                    item.insertText = item.label + "::class"
+                }
+                result.push(item)
+            }
+        });
+        return result
+    }
+}
