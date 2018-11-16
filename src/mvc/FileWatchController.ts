dfcc86d15c42c300986d7b742845afc16ca9a832
Axel VEBER <axel.veber@gmail.com>
2018-11-17 00:44:28 +0100

    Added filter for file watching

--- src/mvc/FileWatchController.ts
+++ src/mvc/FileWatchController.ts
@@ -4,18 +4,28 @@ import { ContainerStore } from "../symfony/ContainerStore";
 export class FileWatchController {
     private _containerStore: ContainerStore
     private _disposable: vscode.Disposable
+    private _configuration = vscode.workspace.getConfiguration("symfony-vscode")
 
     constructor(containerStore: ContainerStore) {
         this._containerStore = containerStore
+        let fileNameRegExp = this._getFileNameRegExp()
 
         let subscriptions: vscode.Disposable[] = []
         vscode.workspace.onDidSaveTextDocument(e => {
-            if(e.fileName.match(/(.yml|.yaml)$/)) {
+            if(e.fileName.match(fileNameRegExp)) {
                 this._containerStore.refreshAll()
             }
         }, this, subscriptions)
 
         this._disposable = vscode.Disposable.from(...subscriptions)
+    }
+
+    protected _getFileNameRegExp(): RegExp {
+        let extensions: string[] = this._configuration.get('fileWatchingPatterns')
+        extensions = extensions.map((ext) => {
+            return '.' + ext
+        })
+        return new RegExp("(" + extensions.join('|') + ")$")
     }
 
     public dispose() {
